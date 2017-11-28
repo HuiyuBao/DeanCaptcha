93e85d0b69a2baabf4ab7c0cd3830995a37ddd06
HuiyuBao <baohuiyu@pku.edu.cn>
2017-11-28 22:37:18 +0800

    finish img2str, remove whitespace.

--- gif2vector.py
+++ gif2vector.py
@@ -1,4 +1,6 @@
 from PIL import Image
+import numpy
+# test
 def gif2chars(file_path):
     '''get the individual images from gif file_path
     Read gif file from file path, extract the four individual characters from the image and return four individual pictures, as a list of pillow image objects. Note the size (width * height) of the image is NOT CHANGED, meaning lots of whitespace remains. Also, the RGB color information is stripped(TODO: is it really true?)
@@ -24,15 +26,86 @@ def remove_redunant_whitespace(pillow_img):
     char_color= colors[0][1]
     width, height = pillow_img.size
     pixel_values = list(pillow_img.getdata())
-    #
 
-    print(pixel_values)
-    return
+    left, right, up, down = 0, 0, 0, 0
+
+    for i in range(height):
+        flag = 1
+        for j in range(width):
+            if pixel_values[i*width+j] == char_color:
+                flag = 0
+        if flag == 0:
+            up = i
+            break
+    for i in range(height - 1, -1, -1):
+        flag = 1
+        for j in range(width):
+            if pixel_values[i*width+j] == char_color:
+                flag = 0
+        if flag == 0:
+            down = i
+            break
+    for j in range(width):
+        flag = 1
+        for i in range(height):
+            if pixel_values[i*width+j] == char_color:
+                flag = 0
+        if flag == 0:
+            left = j
+            break
+    for j in range(width - 1, -1, -1):
+        flag = 1
+        for i in range(height):
+            if pixel_values[i*width+j] == char_color:
+                flag = 0
+        if flag == 0:
+            right = j
+            break
+
+    # let the size be height = 12 and width = 10
+    if (up > 0) and ((up + 10) < height):
+        up = up -1
+        down = up + 11
+    elif up == 0:
+        down = 11
+    else:
+         up = down - 11
+
+    if (left > 1) and ((left)+7 < width):
+        left = left - 2
+        right = left + 9
+    elif (left == 0) or (left == 1):
+        right = left + 9
+    else:
+        left = right -9
+    '''
+        for i in range(up, down + 1, 1):
+            for j in range(left, right , 1):
+                print(pixel_values[i*width+j],end="")
+            print(pixel_values[i*width+right])
+
+        print("\n")
+    '''
+    box = (left, up, right + 1, down + 1)
+    transformed_img = pillow_img.crop(box)
+
+    return left, transformed_img
 
 def img2str(img):
-    return
+    n_array = numpy.array(img.getdata())
+    #print (n_array)
+    return n_array
 
 def gif2charvectors(file_path):
     character = gif2chars(file_path)
-    remove_redunant_whitespace(character[0])
+    chars = list()
+    ans = list()
+    for i in range(4):
+        chars.append(remove_redunant_whitespace(character[i]))
+    list.sort(chars)
+
+    for i in range(4):
+        ans.append(img2str(chars[i][1]))
+    print(ans)
+
 gif2charvectors("test.gif")
