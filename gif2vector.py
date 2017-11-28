926dbe7a7d5121a9df763500958282904d9b0d38
Meng Yibai <mengyibai@pku.edu.cn>
2017-11-28 19:42:53 +0800

    refactored gif2chars for readabilty, added comments.

--- gif2vector.py
+++ gif2vector.py
@@ -1,27 +1,30 @@
 from PIL import Image
 def gif2chars(file_path):
+    '''get the individual images from gif file_path
+    Read gif file from file path, extract the four individual characters from the image and return four individual pictures, as a list of pillow image objects. Note the size (width * height) of the image is NOT CHANGED, meaning lots of whitespace remains. Also, the RGB color information is stripped(TODO: is it really true?)
+    '''
     im = Image.open(file_path)
     colors = im.getcolors()
     list.sort(colors,reverse=True)
-    char_colors = [colors[1][1],colors[2][1],colors[3][1],colors[4][1]]
-    character = list()
+    char_colors = list()
+    for i in range(4):
+        char_colors.append(colors[i+1][1])
+    characters = list()
     for j in range(4):
-        character.append(im.point(lambda i: i == int(char_colors[j])))
-    most_color = list()
-    least_color = list()
-    for j in range(4):
-        temp_im = character[j]
-        colors = temp_im.getcolors()
-        list.sort(colors)
-        most_color.append(colors[1][1])
-        least_color.append(colors[0][1])
-    return character, most_color, least_color
+        characters.append(im.point(lambda i: i == int(char_colors[j])))
+    # a simple algorithm: pick the four most common colors, apart from the background. Vola! pick all pixels of each color, thus getting the four images.
+    return characters
 def remove_redunant_whitespace(pillow_img):
+    '''removes redunant whitespace and resize image of individual characters
+    removes whitespace and turns the size into 16*16.Returns a pillow image object, 16 by 16 TODO: check if this size is necessary
+    '''
+    colors = pillow_img.getcolors()
+    list.sort(colors)
+    background_color = colors[1][1]
+    char_color= colors[0][1]
     width, height = pillow_img.size
-    print(width, height)
     pixel_values = list(pillow_img.getdata())
-
-
+    #
 
     print(pixel_values)
     return
@@ -30,6 +33,6 @@ def img2str(img):
     return
 
 def gif2charvectors(file_path):
-    character, most_color, least_color = gif2chars(file_path)
+    character = gif2chars(file_path)
     remove_redunant_whitespace(character[0])
 gif2charvectors("test.gif")
